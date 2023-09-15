6f51ab96348ab96dda9b50a634e9705d420b7bfd
Vania Gomes - Bora Codar <vania.vgomes@mercadolivre.com>
2023-09-15 18:52:22 -0300

    Create component with a paragraph

---
+++ create_component/src/components/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+//import React from 'react';
+
+const Welcome = () => {
+  const nome = 'Marcos';
+
+  return (
+    <div className="welcome-container">
+      <p>{`Seja bem vindo ${nome}`}</p>
+    </div>
+  )
+
+}
+
+export default Welcome;
