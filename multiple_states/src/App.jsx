76c568e3555ff3eefbdda7cbf28fd628b6be7616
Vania Gomes - Let's Coding <102826843+gomesvania@users.noreply.github.com>
2023-10-30 19:51:32 -0300

    Using multiple State Hook

---
+++ multiple_states/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Toggle from './components/Toggle'
+
+function App() {
+  
+  return (
+    <>
+      <h1>1 - Usando o Estado</h1>
+      <Toggle />
+    </>
+  );
+}
+
+export default App
