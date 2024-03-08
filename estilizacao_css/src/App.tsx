56d0480bb0ec4e44b3480cd8ea5299050b1bede9
Vania Gomes - Let's Coding <102826843+gomesvania@users.noreply.github.com>
2024-03-08 18:35:11 -0300

    Add App.tsx file

--- estilizacao_css/src/App.tsx
+++ estilizacao_css/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Button from "./components/Button";
+import CardSass from "./components/CardSass";
 import HelloWord from "./components/HelloWord";
+import StyledComponent from "./components/StyledComponent";
 import Theme from "./components/Theme";
 
 function App() {
@@ -18,6 +20,10 @@ function App() {
       <Button />
       <h2>Tema claro ou escuro</h2>
       <Theme />
+      <h2>Styled Componentes</h2>
+      <StyledComponent />
+      <h2>Card com Sass</h2>
+      <CardSass title="Titúlo do Card" body="Hello Word" />
     </div>
   );
 }
